82e310c457f310b231bc9751b05a9ed9f4ac723c
Matti Luukkainen <mluukkai@iki.com>
2013-02-16 13:48:35 +0200

    js-based implementation for vertical columns

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require_tree .
 
-$(document).ready(function(){
+$(document).ready(function () {
     $(".review-form").hide();
     $(".reviews").hide();
 
@@ -28,4 +28,39 @@ $(document).ready(function(){
 
     $(".button#review-form").click(toggle_review_form);
     $(".button#review").click(toggle_reviews);
+
+    $('table').rotateTableCellContent();
 });
+
+(function ($) {
+  $.fn.rotateTableCellContent = function (options) {
+  /*
+  Version 1.0
+  7/2011
+  Written by David Votrubec (davidjs.com) and
+  Michal Tehnik (@Mictech) for ST-Software.com
+  */
+
+		var cssClass = ((options) ? options.className : false) || "vertical";
+
+		var cellsToRotate = $('.' + cssClass, this);
+
+		var betterCells = [];
+		cellsToRotate.each(function () {
+			var cell = $(this)
+		  , newText = cell.text()
+		  , height = cell.height()
+		  , width = cell.width()+20
+		  , newDiv = $('<div>', { height: width, width: height })
+		  , newInnerDiv = $('<div>', { text: newText, 'class': 'rotated' });
+
+			newDiv.append(newInnerDiv);
+
+			betterCells.push(newDiv);
+		});
+
+		cellsToRotate.each(function (i) {
+			$(this).html(betterCells[i]);
+		});
+	};
+})(jQuery);
