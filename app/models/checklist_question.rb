6deb6a0fce2be782604762681c80a45a0b78921c
David Consuegra <david@>
2013-11-13 03:06:24 +0200

    Fixed some ordering stuff

--- app/models/checklist_question.rb
+++ app/models/checklist_question.rb
@@ -3,5 +3,4 @@ class ChecklistQuestion < ActiveRecord::Base
   has_many :answers, class_name: 'ChecklistAnswer', dependent: :destroy, autosave: true
   belongs_to :scoretype
   belongs_to :checklist
-  default_scope { order('ordering asc')}
 end
