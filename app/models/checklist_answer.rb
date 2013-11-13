e9801c6d284abbc617c06b1183cb25aaf8259225
David Consuegra <david@>
2013-11-13 03:06:24 +0200

    Fixed some ordering stuff

--- app/models/checklist_answer.rb
+++ app/models/checklist_answer.rb
@@ -1,5 +1,5 @@
 class ChecklistAnswer < ActiveRecord::Base
   attr_accessible :answer, :varname, :value, :feedback, :missing_feedback
   belongs_to :question, class_name: 'ChecklistQuestion', foreign_key: 'checklist_question_id'
-  default_scope { order('ordering asc')}
+  default_scope :order => "ordering"
 end
