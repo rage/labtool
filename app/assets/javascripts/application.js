40761aac51eec3bd499aa13e280fc17a54d7946a
David Consuegra <david@>
2013-11-19 16:45:10 +0200

    More colors!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,7 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the // the compiled file.
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
@@ -76,13 +75,13 @@ $(document).ready(function () {
     if (document.getElementById("questions")) {
       var editor = CodeMirror.fromTextArea(document.getElementById("questions"), {
         lineNumbers: true,
-        mode: "text/yaml",
+        mode: "text/x-yaml",
         vimMode: true,
         showCursorWhenSelecting: true
       });
       var editor2 = CodeMirror.fromTextArea(document.getElementById("checklist_remarks"), {
         lineNumbers: true,
-        mode: "text/markdown",
+        mode: "text/x-markdown",
         vimMode: true,
         showCursorWhenSelecting: true
       });
